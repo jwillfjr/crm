5256f988e23a06bc878017d9efa9cdbb9c1b538b
unknown <jwill.fjr@gmail.com>
2020-08-20 13:09:03 -0300

    remove user & pass

--- packages/server/app/config/db.config.js
+++ packages/server/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://crm:crm123456@cluster0.2iobp.mongodb.net/crm?retryWrites=true&w=majority"
+  url: "mongodb+srv://<user>:<password>@cluster0.2iobp.mongodb.net/crm?retryWrites=true&w=majority"
 };
